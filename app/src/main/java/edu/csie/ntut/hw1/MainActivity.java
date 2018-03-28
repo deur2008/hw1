4ebd906da76ea334f458e915d9dcba36a7879e71
deur2008 <deur2010@hotmail.com>
2018-03-28 22:05:39 +0800

    APP已完成 還未做unit test

--- app/src/main/java/edu/csie/ntut/hw1/MainActivity.java
+++ app/src/main/java/edu/csie/ntut/hw1/MainActivity.java
@@ -3,74 +3,91 @@ package edu.csie.ntut.hw1;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.NumberPicker;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
-
-    private EditText mEdtSex, mEdtAge;
-    private TextView mTxtR;
+    private Spinner mSpnSex;
+    private RadioGroup mRadGrp;
+    private RadioButton mRadBtnAgeRange1;
+    private RadioButton mRadBtnAgeRange2;
+    private RadioButton mRadBtnAgeRange3;
+    private TextView mTxtNumFamily;
+    private NumberPicker mNumPkrFamily;
     private Button mBtnOK;
+    private TextView mTxtSug;
 
-    private String strSug,printOut;
+    private String spnSelected;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        //Toast toast = Toast.makeText(this, "Toast置中顯示", Toast.LENGTH_LONG);
-        mEdtSex = (EditText) findViewById(R.id.edtSex);
-        mEdtAge = (EditText) findViewById(R.id.edtAge);
-        mTxtR = (TextView) findViewById(R.id.txtR);
+        mSpnSex = (Spinner) findViewById(R.id.spnSex);
+        mRadGrp = (RadioGroup) findViewById(R.id.radGrpAge);
+        mRadBtnAgeRange1 = (RadioButton) findViewById(R.id.ageRange1);
+        mRadBtnAgeRange2 = (RadioButton) findViewById(R.id.ageRange2);
+        mRadBtnAgeRange3 = (RadioButton) findViewById(R.id.ageRange3);
+        mTxtNumFamily = (TextView) findViewById(R.id.FamilyMemberText);
+        mNumPkrFamily = (NumberPicker) findViewById(R.id.NumberPicker);
+        mNumPkrFamily.setMinValue(1);
+        mNumPkrFamily.setMaxValue(20);
+        mNumPkrFamily.setValue(3);
         mBtnOK = (Button) findViewById(R.id.btnOK);
-
-        strSug = getString(R.string.suggestion);
+        mTxtSug = (TextView) findViewById(R.id.txtSug);
+        mSpnSex.setOnItemSelectedListener(spnOnItemSelect);
+        mNumPkrFamily.setOnValueChangedListener(numPkrFamilyOnValueChange);
         mBtnOK.setOnClickListener(btnOkOnClick);
     }
 
 
-    private View.OnClickListener btnOkOnClick = new View.OnClickListener() {
+    private AdapterView.OnItemSelectedListener spnOnItemSelect = new AdapterView.OnItemSelectedListener() {
         @Override
-        public void onClick(View v) {
-            String strSex = mEdtSex.getText().toString();
-
-            if (strSex.equals(getString(R.string.male))||strSex.equals(getString(R.string.male_ch)))
-                AgeRange(30, 35);
-            else if (strSex.equals(getString(R.string.female))||strSex.equals(getString(R.string.female_ch)))
-                AgeRange(28, 32);
-            else
-            {
-                printOut = strSug;
-                Toast.makeText(v.getContext(),  getString(R.string.input_error_sex), Toast.LENGTH_SHORT).show();
+        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+            spnSelected = parent.getSelectedItem().toString();
+            if (spnSelected==getString(R.string.male)) {
+                mRadBtnAgeRange1.setText(getString(R.string.below30));
+                mRadBtnAgeRange2.setText(getString(R.string.between30to40));
+                mRadBtnAgeRange3.setText(getString(R.string.over40));
             }
+            else if (spnSelected==getString(R.string.female)){
+                mRadBtnAgeRange1.setText(getString(R.string.below28));
+                mRadBtnAgeRange2.setText(getString(R.string.between28to35));
+                mRadBtnAgeRange3.setText(getString(R.string.over35));
+            }
+        }
 
+        @Override
+        public void onNothingSelected(AdapterView<?> parent) {
 
-            mTxtR.setText(printOut);
         }
     };
 
-    private void AgeRange(int not_hurry, int get_married) {
-
-        //int inputAge = Integer.parseInt(mEdtAge.getText().toString());
-        String inputAge = mEdtAge.getText().toString();
-        try {
-            int num = Integer.parseInt(inputAge);
-
-            if (num < not_hurry)
-                printOut = strSug+getString(R.string.not_hurry);
-            else if (num > get_married)
-                printOut = strSug+getString(R.string.get_married);
-            else
-                printOut = strSug+getString(R.string.find_couple);
+    private NumberPicker.OnValueChangeListener numPkrFamilyOnValueChange = new NumberPicker.OnValueChangeListener() {
+        @Override
+        public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+            mTxtNumFamily.setText(String.valueOf(newVal));
         }
-        catch (NumberFormatException e)
-        {
-            printOut = strSug;
-            Toast.makeText(this, getString(R.string.input_error_age), Toast.LENGTH_SHORT).show();
-        }
+    };
 
-    }
+    private View.OnClickListener btnOkOnClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            MarriageSuggestion ms = new MarriageSuggestion();
+            String strSex = mSpnSex.getSelectedItem().toString();
+            int iAgeRange = 0;
+            if (mRadGrp.getCheckedRadioButtonId()== R.id.ageRange1) iAgeRange = 1;
+            else if (mRadGrp.getCheckedRadioButtonId()== R.id.ageRange2) iAgeRange = 2;
+            else if (mRadGrp.getCheckedRadioButtonId()== R.id.ageRange3) iAgeRange = 3;
+            else Toast.makeText(v.getContext(), getString(R.string.input_error_age), Toast.LENGTH_SHORT).show();
+            mTxtSug.setText(ms.getSuggestion(strSex, iAgeRange, mNumPkrFamily.getValue()));
+        }
+    };
+
 }
---
+++ app/src/main/java/edu/csie/ntut/hw1/MarriageSuggestion.java
@@ -0,0 +1,53 @@
+package edu.csie.ntut.hw1;
+
+
+import android.content.res.Resources;
+
+public class MarriageSuggestion {
+
+    public String getSuggestion(String strSex, int iAgeRange, int numFamily) {
+
+        String strSug = "建議： ";
+        String get_married = "趕快結婚！";
+        String find_couple = "開始找對象";
+        String not_hurry = "還不急";
+        
+        if (strSex.equals("男")) {
+            if (iAgeRange==1) {
+                    if (numFamily < 4)  strSug += get_married;
+                    else if (numFamily >= 4 && numFamily <= 10) strSug += get_married;
+                    else    strSug += not_hurry;
+            }
+            else if(iAgeRange == 2) {
+                if (numFamily < 4)  strSug += get_married;
+                else if (numFamily >= 4 && numFamily <= 10) strSug += find_couple;
+                else    strSug += not_hurry;
+            }
+            else if(iAgeRange == 3) {
+                if (numFamily < 4)  strSug += find_couple;
+                else if (numFamily >= 4 && numFamily <= 10) strSug += get_married;
+                else    strSug += find_couple;
+            }
+        }
+        else
+        {
+            if (iAgeRange==1) {
+                if (numFamily < 4)  strSug += get_married;
+                else if (numFamily >= 4 && numFamily <= 10) strSug += get_married;
+                else    strSug += not_hurry;
+            }
+            else if(iAgeRange == 2) {
+                if (numFamily < 4)  strSug += get_married;
+                else if (numFamily >= 4 && numFamily <= 10) strSug += find_couple;
+                else    strSug += not_hurry;
+            }
+            else if(iAgeRange == 3) {
+                if (numFamily < 4)  strSug += find_couple;
+                else if (numFamily >= 4 && numFamily <= 10) strSug += get_married;
+                else    strSug += find_couple;
+            }
+        }
+
+        return strSug;
+    }
+}
